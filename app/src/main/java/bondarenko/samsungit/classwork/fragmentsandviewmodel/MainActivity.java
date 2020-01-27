bba2e8a75411e2a4bc7e3250f271bdeb27cd9f32
denbondd <Denbusiness#1>
2020-01-27 14:48:51 +0200

    FirstCommit

---
+++ app/src/main/java/bondarenko/samsungit/classwork/fragmentsandviewmodel/MainActivity.java
@@ -0,0 +1,13 @@
+package bondarenko.samsungit.classwork.fragmentsandviewmodel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
